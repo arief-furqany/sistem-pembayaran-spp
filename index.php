be4f6c966889e2f02afa17a64c2531e939f82236
Muhammad Arief Furqany <m.arief.furqany@gmail.com>
2024-12-12 18:14:09 +0700

    Penambahan halaman login dan perbaikan halaman landing page

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
+
 <?php
 include 'includes/header.php';
 include 'includes/db.php';
 
-// Fetch data from the database
+$student_id = isset($_GET['student_id']) ? $_GET['student_id'] : null;
+if ($student_id) {
+    $query = "SELECT * FROM students WHERE id = $student_id";
+    $result = mysqli_query($conn, $query);
+    $student = mysqli_fetch_assoc($result);
+}
+
+// Fetch all data for admin view
 $query = "SELECT * FROM students";
-$result = mysqli_query($conn, $query);
+$result_all = mysqli_query($conn, $query);
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +23,10 @@ $result = mysqli_query($conn, $query);
 </head>
 <body>
     <div class="container">
-        <h1>Selamat Datang di Sistem Pembayaran SPP</h1>
+        <h1>Welcome to SPP Payment System</h1>
+        <?php if ($student_id && $student): ?>
+            <h2>Hello, <?php echo $student['name']; ?></h2>
+        <?php endif; ?>
         <table>
             <thead>
                 <tr>
@@ -27,14 +38,18 @@ $result = mysqli_query($conn, $query);
                 </tr>
             </thead>
             <tbody>
-                <?php while($row = mysqli_fetch_assoc($result)): ?>
+                <?php while($row = mysqli_fetch_assoc($result_all)): ?>
                     <tr>
                         <td><?php echo $row['name']; ?></td>
                         <td><?php echo $row['nisn']; ?></td>
                         <td><?php echo $row['class']; ?></td>
                         <td><?php echo $row['payment_status'] ? 'Paid' : 'Unpaid'; ?></td>
                         <td>
-                            <a href="payment_menu.php?student_id=<?php echo $row['id']; ?>" class="btn">Bayar</a>
+                            <?php if (!$row['payment_status']): ?>
+                                <a href="payment_menu.php?student_id=<?php echo $row['id']; ?>" class="btn">Pay</a>
+                            <?php else: ?>
+                                <span class="btn disabled">Paid</span>
+                            <?php endif; ?>
                         </td>
                     </tr>
                 <?php endwhile; ?>
